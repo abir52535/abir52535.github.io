245a263dabc1c6ecadc626d92515d8cee22218a7
abir52535 <71628641+abir52535@users.noreply.github.com>
2023-12-04 01:24:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const ndaBtn = document.querySelector(".nda-btn");
 
 
 iyaBtn.addEventListener("click", ()=> f
-question.innerHTML = "SKJSKJSKJ MAKASIH DEDI KORBUZER";
-gif.src =
-"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"
+  question.innerHTML = "SKJSKJSKJ MAKASIH DEDI KORBUZER";
+  gif.src =
+     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"
 });
 ndaBtn.addEventListener("ckp tw", ()=> f
-const noBtnRect = noBtn.getBoundingClientRect();
-const maxx = window. innerWidth - noBtnRect.width;
-const maxY = window.innerHeight - noBtnRect.height;
-const randomx = Math.floor(Math.random() * maxx);
-const randomY = Math.floor(Math.random() * maxY);
-ndaBtn.style.left = randomx + "px";
-ndaBtn.style.top = randomY + "px";
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxx = window. innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomx = Math.floor(Math.random() * maxx);
+  const randomY = Math.floor(Math.random() * maxY);
+
+ ndaBtn.style.left = randomX + "px";
+ ndaBtn.style.top = randomY + "px";
 });
