efdeb5d3e024741a64173b15991c66001c009e3d
abir52535 <71628641+abir52535@users.noreply.github.com>
2023-12-04 01:04:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.queryselector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
+const iyaBtn = document.querySelector(".yes-btn");
+const ndaBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", ()=> f
-question.innerHTML = "Aaaaa, I like you too";
+question.innerHTML = "SKJSKJSKJ MAKASIH DEDI KORBUZER";
 gif.src
 1
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 H;
-noBtn.addEventListener("mouseover", ()=> f
+noBtn.addEventListener("ckp tw", ()=> f
 const noBtnRect = noBtn.getBoundingClientRect();
 const maxx = window. innerWidth - noBtnRect.width;
 const maxY = window.innerHeight - noBtnRect.height;
