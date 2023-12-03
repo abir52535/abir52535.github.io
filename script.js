137d60b5cff4a1dac5e266ca8f58381a60b4a442
abir52535 <71628641+abir52535@users.noreply.github.com>
2023-12-04 00:55:29 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.queryselector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", ()=> f
+question.innerHTML = "Aaaaa, I like you too";
+gif.src
+1
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+H;
+noBtn.addEventListener("mouseover", ()=> f
+const noBtnRect = noBtn.getBoundingClientRect();
+const maxx = window. innerWidth - noBtnRect.width;
+const maxY = window.innerHeight - noBtnRect.height;
+const randomx = Math.floor(Math.random() * maxx);
+const randomY = Math.floor(Math.random() * maxY);
+noBtn.style.left = randomx + "px";
+noBtn.style.top = randomY + "px";
+H;
