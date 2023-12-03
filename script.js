29f68c261a9521c1e85f91ea1e13d9a87c9a9fc5
abir52535 <71628641+abir52535@users.noreply.github.com>
2023-12-04 01:11:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const question = document.querySelector(".question");
 const gif = document.queryselector(".gif");
 const iyaBtn = document.querySelector(".iya-btn");
 const ndaBtn = document.querySelector(".nda-btn");
-yesBtn.addEventListener("click", ()=> f
+iyaBtn.addEventListener("click", ()=> f
 question.innerHTML = "SKJSKJSKJ MAKASIH DEDI KORBUZER";
 gif.src
 1
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 H;
-noBtn.addEventListener("ckp tw", ()=> f
+ndaBtn.addEventListener("ckp tw", ()=> f
 const noBtnRect = noBtn.getBoundingClientRect();
 const maxx = window. innerWidth - noBtnRect.width;
 const maxY = window.innerHeight - noBtnRect.height;
 const randomx = Math.floor(Math.random() * maxx);
 const randomY = Math.floor(Math.random() * maxY);
-noBtn.style.left = randomx + "px";
-noBtn.style.top = randomY + "px";
+ndaBtn.style.left = randomx + "px";
+ndaBtn.style.top = randomY + "px";
 H;
